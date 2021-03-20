58b6ce3581c6c420c34d9be432b7a7f5dbe7774c
Jonas <jonafronja@hotmail.com>
2021-03-20 10:48:44 -0300

    buscador de gif

---
+++ android/app/src/main/kotlin/fronja/gif/MainActivity.kt
@@ -0,0 +1,6 @@
+package fronja.gif
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:gif/ui/home_page.dart';
+import 'package:gif/ui/gif_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
+
+
